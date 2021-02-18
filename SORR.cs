c66114bacf12bb89b38e78a4a603d8d6e99a8a8c
StarryFeline443 <57163449+StarryFeline443@users.noreply.github.com>
2021-02-18 11:28:39 -0700

    Add files via upload

---
+++ SORR.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace SORR
+{
+	public class SORR : Mod
+	{
+	}
+}
---
+++ SORRPlayer.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameInput;
+using SORR.buffs.Debuffs;
+
+namespace SORR
+{
+    public class SORRPlayer : ModPlayer
+    {
+
+
+
+        public bool ArgentBurns = false;
+        public bool KiskaBuff;
+        
+        public override void ResetEffects()
+        {
+            ArgentBurns = false;
+        }
+
+        public override void UpdateBadLifeRegen()
+        {
+            if (ArgentBurns)  // make sure you add the right bool 
+            {
+                player.lifeRegen -= 75; //this make so the player take damage, the highter is the value the more life losing.
+            }
+        }
+
+    }
+}
